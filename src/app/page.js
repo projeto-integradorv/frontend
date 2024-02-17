46f18469a248395b853a037b1794466d4af9964a
LivramentoAmaral <marcosdoamaral2002@gmail.com>
2024-02-17 11:37:57 -0300

    começando a mexer no forntl

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          BEm vindo ao litmach&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
