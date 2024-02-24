9bd5df0ff3975c6309da3d72dae8eddc3ff4297c
LivramentoAmaral <marcosdoamaral2002@gmail.com>
2024-02-24 17:31:00 -0300

    criando a navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,13 +1,21 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
-
-
 export default function Navbar() {
     return (
-        <Container maxWidth="sm">
-            <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+        <Container maxWidth={false} disableGutters={true} >
+            <Box sx={{
+                bgcolor: '#FF9800', height: '45vh', width: '100%', display: 'flex', flexDirection: 'column',
+                justifyContent: 'flex-end', alignItems: 'center'
+            }} >
+                <h1 style={{
+                    color: 'white', height: '10vh'
+                    , display: 'flex', flexDirection: 'column',
+                    width: "70%", height: "22vh"
+                    , alignItems: 'self-start', justifyContent: 'flex-start'
+                    , fontSize: '45px'
+                }}><span style={{fontSize:'35px'}}>Olá</span>Seja Bem-vindo ao Litmach</h1>
+            </Box>
         </Container>
-    )
-
+    );
 }
