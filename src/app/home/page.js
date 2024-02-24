d28cabfa42ece92aa4d849ce88349b47c96c8291
Wellington Santos <wellingtonasc38@gmail.com>
2024-02-24 16:19:19 -0300

    mui

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,11 +1,10 @@
-
+import Navbar from "@/components/navbar"
 
 export default function Home() {
 
     return (
         <div>
-        <h1>Home</h1>
-        <p>Seja bem-vindo ao sistema</p>
+            <Navbar />
         </div>
     )
 }
---
+++ src/components/navbar/index.js
@@ -0,0 +1,13 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+
+
+export default function Navbar() {
+    return (
+        <Container maxWidth="sm">
+            <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+        </Container>
+    )
+
+}
