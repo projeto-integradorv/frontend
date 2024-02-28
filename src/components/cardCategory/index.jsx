4dd8257f683d74da5d01b58c786691f3a95b2cd5
LivramentoAmaral <marcosdoamaral2002@gmail.com>
2024-02-28 19:05:54 -0300

    criando o componente CardCategory'

--- src/components/cardCategory/index.jsx
+++ src/components/cardCategory/index.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Button } from "@mui/material";
+import Image from 'next/image';
 
 
 
-export default function CardCategory() {
+export default function CardCategory({ icon , nameCategory}) {
     return (
-        <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%', padding: '10px', gap: '10px' }}>
+        <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%', padding: '10px', gap: '10px',flexBasis:'calc(16.666% - 20px)',flexGrow:'1',margin:'10px'}}>
             <Image
-                src={MassasImg}
-                alt="Massas"
+                src={icon}
                 width={'100%'}
                 height={'100%'}
                 sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
             />
-            <p style={{ color: '#665858' }}>Massas</p>
+            <p style={{ color: '#665858' }}>{nameCategory}</p>
         </Button>
     )
 }
--- src/components/categoria/index.jsx
+++ src/components/categoria/index.jsx
@@ -1,141 +1,27 @@
-import { Container, Button } from "@mui/material";
-import Image from 'next/image';  // Import Next.js Image component
-import MassasImg from '../../assets/Vector.png';
-import LanchesImg from '../../assets/lanches.png';
+import { Container, Button, Card } from "@mui/material";
+import CardCategory from "../cardCategory";
 import React from "react";
+import ImageMassa from '../../assets/Vector.png';
+import Imagelanches from '../../assets/lanches.png';
 
 export default function Categoria() {
     return (
-        <Container maxWidth='xl' sx={{ backgroundColor: 'transparent', height: '100%', display: 'flex', flexDirection: 'column',justifyContent:'flex-start' }} disableGutters={true}>
-            <Container maxWidth='xl' sx={{ backgroundColor: 'transparent', height: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '1%' }} disableGutters={true}>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%', padding:'10px' ,gap:'10px'}}>
-                    <Image
-                        src={MassasImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%', padding:'10px',gap:'10px' }}>
-                    <Image
-                        src={MassasImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%',padding:'10px',gap:'10px' }}>
-                    <Image
-                        src={MassasImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%',padding:'10px',gap:'10px' }}>
-                    <Image
-                        src={MassasImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%',padding:'10px' ,gap:'10px'}}>
-                    <Image
-                        src={MassasImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%', padding:'10px' ,gap:'10px'}}>
-                    <Image
-                        src={MassasImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-
-
+        <Container maxWidth='md' sx={{ backgroundColor: 'transparent', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
+            <Container maxWidth='xl' sx={{ backgroundColor: 'transparent', height: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '1%',flexWrap:'wrap', }} disableGutters={true}>
+                <CardCategory icon={ImageMassa} nameCategory={'Massas'}/>
+                <CardCategory icon={ImageMassa} nameCategory={'Massas'}/>
+                <CardCategory icon={ImageMassa} nameCategory={'Massas'}/>
+                <CardCategory icon={ImageMassa} nameCategory={'Massas'}/>
+                <CardCategory icon={ImageMassa} nameCategory={'Massas'}/>
+                <CardCategory icon={ImageMassa} nameCategory={'Massas'}/>
+                <CardCategory icon={Imagelanches} nameCategory={'Lanches'}/>
+                <CardCategory icon={Imagelanches} nameCategory={'Lanches'}/>
+                <CardCategory icon={Imagelanches} nameCategory={'Lanches'}/>
+                <CardCategory icon={Imagelanches} nameCategory={'Lanches'}/>
+                <CardCategory icon={Imagelanches} nameCategory={'Lanches'}/>
+                <CardCategory icon={Imagelanches} nameCategory={'Lanches'}/>
+            
             </Container>
-            <Container maxWidth='xl' sx={{ backgroundColor: 'transparent', height: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '1%' }} disableGutters={true}>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%' ,padding:'10px',gap:'10px'}}>
-                    <Image
-                        src={LanchesImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%',padding:'10px' ,gap:'10px'}}>
-                    <Image
-                        src={LanchesImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%',padding:'10px',gap:'10px' }}>
-                    <Image
-                        src={LanchesImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%',padding:'10px',gap:'10px' }}>
-                    <Image
-                        src={LanchesImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%',padding:'10px',gap:'10px' }}>
-                    <Image
-                        src={LanchesImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-                <Button href="#contained-buttons" sx={{ width: '10%', height: '16%', backgroundColor: 'white', display: 'flex', flexDirection: 'column', marginRight: '1%',padding:'10px' ,gap:'10px'}}>
-                    <Image
-                        src={LanchesImg}
-                        alt="Massas"
-                        width={'100%'}
-                        height={'100%'}
-                        sx={{ borderRadius: '50%', backgroundColor: '#665858' }}
-                    />
-                    <p style={{ color: '#665858' }}>Massas</p>
-                </Button>
-
-
-            </Container>
-
         </Container>
     );
 }
