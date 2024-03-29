2bcb449b8da7751f4da44374f2ce2fb4c34ce6c9
LivramentoAmaral <marcosdoamaral2002@gmail.com>
2024-03-29 13:53:04 -0300

    alteração

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,18 +5,20 @@ import { HOST_API } from '@/config-global';
 // ----------------------------------------------------------------------
 
 const axiosInstance = axios.create(
-    { 
+    {
         baseURL: HOST_API,
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Token 0be0a6499b1aaa2ae6fa7471bd428ffb1df1cdf0`
+            'Authorization': `Token 0be0a6499b1aaa2ae6fa7471bd428ffb1df1cdf0`,
+            "key": "Access-Control-Allow-Origin",
+            "value": "http://wellington.thunder.dev.br"
         }
     }
 );
 
 axiosInstance.interceptors.response.use(
-  (res) => res,
-  (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
+    (res) => res,
+    (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
 );
 
 export default axiosInstance;
