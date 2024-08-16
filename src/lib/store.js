92a128e0c38e45a5b5b45a7ffb7c94a8758d59de
Wellington Santos <wellingtonasc38@gmail.com>
2024-08-16 11:24:07 -0300

    fix redux

--- src/lib/store.js
+++ src/lib/store.js
@@ -6,18 +6,24 @@ import loginReducer from "./features/login/loginSlice";
 import categoriasListener from "./features/categoria/middlewares";
 import categoriasSlice from "./features/categoria/categoriaSlice";
 import produtosListener from "./features/produtos/middlewares";
+
+import { adicionalListener } from "./features/adicionais/middlewares";
+import adicionalSlice  from "./features/adicionais/adicionaisSlice";
+
 export const makeStore = () => {
   return configureStore({
     reducer: {
       produtos: produtosSlice,
       carrinho: carrinhoSlice,
       login: loginReducer,
-      categorias: categoriasSlice, 
+      categorias: categoriasSlice,
+      adicionais: adicionalSlice,
     },
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(
-        categoriasListener.middleware, 
-        produtosListener.middleware, 
-          ),
+        categoriasListener.middleware,
+        produtosListener.middleware,
+        adicionalListener.middleware
+      ),
   });
 };
